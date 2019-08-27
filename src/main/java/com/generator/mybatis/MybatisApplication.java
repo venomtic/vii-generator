6359fb723ada843a783ddd38956166e9f179ee9a
liuyuchen <liuyuchen@xinchao.mobi>
2019-08-27 20:55:06 +0800

    初始化项目

---
+++ src/main/java/com/generator/mybatis/MybatisApplication.java
@@ -0,0 +1,39 @@
+package com.generator.mybatis;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class MybatisApplication {
+
+    //    public static void main(String[] args) {
+//        SpringApplication.run(MybatisApplication.class, args);
+//    }
+    public static void main(String[] args) throws URISyntaxException {
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException | IOException | InterruptedException | InvalidConfigurationException | XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/generator/plugin/OverIsMergeablePlugin.java
@@ -0,0 +1,33 @@
+package com.generator.plugin;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * @author yuchen
+ * @date 2019-08-27
+ * @description 解决mapper.xml无法覆盖问题
+ */
+public class OverIsMergeablePlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
