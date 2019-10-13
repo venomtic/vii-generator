fbcbc4fb816ddb576a9e609a6a2bb8f30bfb98ab
liuyuchen <>
2019-10-13 10:42:27 +0800

    增加自定义TypeResolver

---
+++ src/main/java/com/generator/plugin/TypeResolver.java
@@ -0,0 +1,14 @@
+package com.generator.plugin;
+
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
+
+/**
+ * @author yuchen
+ * @date 2019/10/13
+ */
+public class TypeResolver extends JavaTypeResolverDefaultImpl {
+    public TypeResolver() {
+        super.typeMap.put(-6, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("TINYINT", new FullyQualifiedJavaType(Integer.class.getName())));
+    }
+}
